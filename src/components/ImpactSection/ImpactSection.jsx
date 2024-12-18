902916d40e0ebcf8071be568fcce9adc6dd49ea8
Marcus Emmanuel <marcusmanuel597@gmail.com>
2024-12-18 18:31:31 +0100

    message

--- src/components/ImpactSection/ImpactSection.jsx
+++ src/components/ImpactSection/ImpactSection.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./ImpactSection.css"; // Assuming you will define your styles in this file
+import "./ImpactSection.css";
 
 const ImpactSection = () => {
   const [impactData, setImpactData] = useState({
